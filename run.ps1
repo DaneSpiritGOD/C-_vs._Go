90043dfd17e9502c3a13073f8152463771c06679
dane.dai <Dane.dai@wisetechglobal.com>
2021-04-30 17:50:37 +0800

    improve

--- run.ps1
+++ run.ps1
@@ -1,4 +1,17 @@
-Write-Output "lang,iters,ppc,iters*ppc,elapsed time" > result.csv
-
+Write-Host "building go version..."
 go build .\src\go\go.go
-.\go.exe -iters 10 -ppc 1000000 >> result.csv
+
+Write-Host "building c# version..."
+dotnet build -c Release .\src\csharp\csharp.csproj > $null
+
+Write-Host "running debug mode test..."
+
+Write-Output $null > debug.txt
+.\src\go\go.exe -iters 10 -ppc 1000000 -debug >> debug.txt
+.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 1000000 -debug true >> debug.txt
+
+Write-Host "running test..."
+
+Write-Output "lang,iters,ppc,iters*ppc,elapsed time" > result.csv
+.\src\go\go.exe -iters 10 -ppc 1000000 >> result.csv
+.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 1000000 -debug false >> result.csv
--- src/Go/go.go
+++ src/Go/go.go
@@ -7,25 +7,21 @@ import (
 	"time"
 )
 
-func createChan() chan int {
-	return make(chan int)
-}
-
-func syncSingleBenchmark(count int, iteration int, wg *sync.WaitGroup, printDebugInfo bool) {
+func syncSingleBenchmark(count int, iteration int, wg *sync.WaitGroup, debugMode bool) {
 	defer func() {
 		wg.Done()
 	}()
 
 	start := time.Now()
 
-	var output, input chan int = nil, createChan()
+	input := createChan()
 
 	go func(in chan int) {
 		in <- 0
 	}(input)
 
 	for i := 0; i < count; i++ {
-		output = createChan()
+		output := createChan()
 
 		go func(pong, ping chan int) {
 			pong <- 1 + <-ping
@@ -34,35 +30,40 @@ func syncSingleBenchmark(count int, iteration int, wg *sync.WaitGroup, printDebu
 		input = output
 	}
 
-	if printDebugInfo {
-		fmt.Printf("%dth iteration finished: took %s, final value: %d\n", iteration, time.Since(start), <-input)
+	finalValue := <-input
+	if debugMode {
+		fmt.Printf("%dth iteration finished: took %s, final value: %d\n", iteration, time.Since(start), finalValue)
 	}
+}
+
+func createChan() chan int {
+	return make(chan int, 1)
 }
 
 var maxIterationCount = flag.Int("iters", 10, "how many iterations")
 var pingpongCountPerIteration = flag.Int("ppc", 100_0000, "how many pingpong in single iteration")
-var debugMode = flag.Bool("debug", false, "whether to print intermediate information")
+var debugMode = flag.Bool("debug", false, "is debug modeto judge whether to print intermediate information")
 
 func main() {
 	flag.Parse()
 
 	iterationCount := *maxIterationCount
 	pingpongCount := *pingpongCountPerIteration
-	printDebugInfo := *debugMode
+	debugMode2 := *debugMode
 
-	if printDebugInfo {
-		fmt.Printf("Started, will Run %d(iterations) * %d(ppc./iter.) of benchmark.\n", iterationCount, pingpongCount)
+	if debugMode2 {
+		fmt.Printf("Started, will run %d(iterations) * %d(ppc./iter.) of benchmark.\n", iterationCount, pingpongCount)
 	}
 
 	start := time.Now()
 	wg := &sync.WaitGroup{}
 	for i := 0; i < iterationCount; i++ {
 		wg.Add(1)
-		go syncSingleBenchmark(pingpongCount, i, wg, printDebugInfo)
+		go syncSingleBenchmark(pingpongCount, i, wg, debugMode2)
 	}
 	wg.Wait()
 
-	if printDebugInfo {
+	if debugMode2 {
 		fmt.Printf("Finished totally, took %s.\n", time.Since(start))
 	} else {
 		fmt.Printf("%s,%d,%d,%d,%s", "go", iterationCount, pingpongCount, iterationCount*pingpongCount, time.Since(start))
