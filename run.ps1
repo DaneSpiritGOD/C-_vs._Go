0dc868b0ecae359fca9e3ec01d4e653ccb0515a8
dane.dai <Dane.dai@wisetechglobal.com>
2021-05-07 13:20:44 +0800

    improve run script

--- run.ps1
+++ run.ps1
@@ -4,29 +4,47 @@ go build -o .\src\go\go.exe .\src\go\go.go
 Write-Host "building c# version..."
 dotnet build -c Release .\src\csharp\csharp.csproj
 
-Write-Output "debug mode info" > debug.txt
+function Runbenchmark {
+  param(
+    $iters,
+    $ppc,
+    $debug,
+    $goPath,
+    $csharpPath,
+    $output,
+    $brandNewCsv = $false,
+    $appendCsvSepeator = $false
+  )
 
-Write-Host "running debug mode test(go)..."
-.\src\go\go.exe -iters 10 -ppc 1000000 -debug >> debug.txt
+  if ($brandNewCsv) {
+    Write-Output $(if($output.EndsWith(".csv")) { "lang,iters,ppc,iters*ppc,elapsed time" } else { $null }) > $output
+  }
 
-Write-Host "running debug mode test(C# noNewTask)..."
-.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 1000000 -debug true -newTask false >> debug.txt
+  if ($appendCsvSepeator) {
+    Write-Output $(if($output.EndsWith(".csv")) { "-,-,-,-,-" } else { $null }) >> $output
+  }
 
-Write-Host "running debug mode test(C# newTask)..."
-.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 1000000 -debug true -newTask true >> debug.txt
+  Write-Host "running $iters*$ppc test(go)..."
+  &$goPath -iters $iters -ppc $ppc $(if ($debug) { "-debug" }) >> $output
 
-Write-Output "lang,iters,ppc,iters*ppc,elapsed time" > result.csv
+  $csharpDebugValue = if ($debug) { "true" } else { "false" }
 
-Write-Host "running 10*1000000 test(go)..."
-.\src\go\go.exe -iters 10 -ppc 1000000 >> result.csv
-Write-Host "running 10*1000000 test(C# noNewTask)..."
-.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 1000000 -debug false -newTask false >> result.csv
-Write-Host "running 10*1000000 test(C# newTask)..."
-.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 1000000 -debug false -newTask true >> result.csv
+  Write-Host "running $iters*$ppc test(C# noNewTask)..."
+  &$csharpPath -iters $iters -ppc $ppc -debug $csharpDebugValue -newTask false >> $output
 
-Write-Host "running 10*10000000 test(go)..."
-.\src\go\go.exe -iters 10 -ppc 10000000 >> result.csv
-Write-Host "running 10*10000000 test(C# noNewTask)..."
-.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 10000000 -debug false -newTask false >> result.csv
-Write-Host "running 10*10000000 test(C# newTask)..."
-.\src\csharp\bin\Release\net5.0\csharp.exe -iters 10 -ppc 10000000 -debug false -newTask true >> result.csv
+  Write-Host "running $iters*$ppc test(C# newTask)..."
+  &$csharpPath -iters $iters -ppc $ppc -debug $csharpDebugValue -newTask true >> $output
+}
+
+Write-Host "--------------------------------------debug mode--------------------------------------"
+Runbenchmark 10 1000000 $true ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "debug_results.txt" $true
+
+Write-Host "--------------------------------------benchmark mode--------------------------------------"
+Runbenchmark 1 10000000 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $true
+Runbenchmark 10 1000000 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
+Runbenchmark 100 100000 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
+Runbenchmark 1000 10000 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
+Runbenchmark 10000 1000 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
+Runbenchmark 100000 100 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
+Runbenchmark 1000000 10 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
+Runbenchmark 10000000 1 $false ".\src\go\go.exe" ".\src\csharp\bin\Release\net5.0\csharp.exe" "result.csv" $false $true
